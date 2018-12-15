39e73d7beaa53e1066b735fd2f78b3a556651863
sferreira8 <42465693+sferreira8@users.noreply.github.com>
2018-12-15 20:24:05 +0000

    Update .js

--- .js
+++ .js
@@ -1,3 +1,5 @@
+
+//Object.create function to create new objects
 var Pastry={
 	inicializa: function(type,flavor,levels,price,occasion){
 		this.type=type;
@@ -25,7 +27,6 @@ var muffin= Object.create(Pastry);
 muffin.inicializa ("muffin","blueberry",1, "$2","breakfast");
 
 
-
 var cake = Object.create(Pastry);
 cake.inicializa("cake","vanilla",3,"$10","birthday");
 
@@ -35,7 +36,7 @@ console.log(cake.describe());
 console.log(muffin.bake());
 console.log(cake.bake());
 
-
+//constructor function 
 function Pastry (type,flavor,levels,price,occasion){
 	this.type=type;
 	this.flavor=flavor;
@@ -43,5 +44,45 @@ function Pastry (type,flavor,levels,price,occasion){
 	this.price=price;
 	this.occasion=occasion;
 
-	
+//method calls the prototype
+ Pastry.prototype.describe= function(){
+ 	var description= "The " + this.type+ " is a "+ this.occasion+ " pastry, has a " + this.flavor + " flavor,"
+	 + this.levels + " layers(s), and costs "+ this.price+ ".";
+	return description;
+	}	
+
 }
+
+//creating new objects
+var cake = new Pastry("cake","vanilla",3,"$10","birthday");
+var muffin= new Pastry("muffin","blueberry",1, "$2","breakfast");
+console.log(cake);
+console.log(cake.describe());
+
+//local and global scopes 
+var foo = "bar";
+
+function baz() {
+  console.log(foo);
+  console.log(this.foo);
+}
+
+baz();
+// > "bar"
+// > "bar"
+
+
+function baz() {
+  var foo = "bar";
+
+  console.log(foo);
+  console.log(this.foo);
+}
+
+baz();
+// > "bar"
+// > undefined
+
+
+
+
